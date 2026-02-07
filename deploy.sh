30bb693d8fd265b565f4d8f007cd4a4756ebd2b9
robg <17162939+rg-2@users.noreply.github.com>
2026-02-07 09:48:29 -0500

    Added humidity plot to graphs, removed rain and pressure.   split the chown line out, so I can copy/paste individual commands when making small updates.

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,8 @@ sudo cp weewx.conf /etc/weewx/weewx.conf
 sudo cp skin.conf /etc/weewx/skins/Belchertown/skin.conf
 sudo cp graphs.conf /etc/weewx/skins/Belchertown/graphs.conf
 sudo cp sdr.py /etc/weewx/bin/user/sdr.py
-sudo chown root:weewx /etc/weewx/weewx.conf /etc/weewx/skins/Belchertown/skin.conf /etc/weewx/skins/Belchertown/graphs.conf /etc/weewx/bin/user/sdr.py
+sudo chown root:weewx /etc/weewx/weewx.conf
+sudo chown root:weewx /etc/weewx/skins/Belchertown/skin.conf 
+sudo chown root:weewx /etc/weewx/skins/Belchertown/graphs.conf 
+sudo chown root:weewx /etc/weewx/bin/user/sdr.py
 sudo weectl report run
