e6afc0994a8264a692429454fb2e44fcdf511287
robg <17162939+rg-2@users.noreply.github.com>
2026-03-10 11:39:11 -0400

    updated the footer position on the kiosk paget, changed the copyright info.  Added the kiosk css to the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 sudo cp weewx.conf /etc/weewx/weewx.conf
 sudo cp skin.conf /etc/weewx/skins/Belchertown/skin.conf
 sudo cp graphs.conf /etc/weewx/skins/Belchertown/graphs.conf
+sudo cp kiosk.css /etc/weewx/skins/Belchertown/kiosk.css
 sudo cp sdr.py /etc/weewx/bin/user/sdr.py
 sudo chown root:weewx /etc/weewx/weewx.conf
-sudo chown root:weewx /etc/weewx/skins/Belchertown/skin.conf 
-sudo chown root:weewx /etc/weewx/skins/Belchertown/graphs.conf 
+sudo chown root:weewx /etc/weewx/skins/Belchertown/skin.conf
+sudo chown root:weewx /etc/weewx/skins/Belchertown/graphs.conf
+sudo chown root:weewx /etc/weewx/skins/Belchertown/kiosk.css
 sudo chown root:weewx /etc/weewx/bin/user/sdr.py
 sudo systemctl restart weewx
-sudo -u weewx weectl report run
+cd / && sudo -u weewx weectl report run
