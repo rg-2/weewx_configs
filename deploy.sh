f89faaebe8ea5699f5ee6eedb8e2fee15679359b
robg <17162939+rg-2@users.noreply.github.com>
2026-04-06 14:56:05 -0400

    claneup so things fit on the tablet kiosk display

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # ~/projects/weewx_configs/deploy.sh
 #sudo cp weewx.conf /etc/weewx/weewx.conf
-#sudo cp skin.conf /etc/weewx/skins/Belchertown/skin.conf
+sudo cp skin.conf /etc/weewx/skins/Belchertown/skin.conf
 sudo cp graphs.conf /etc/weewx/skins/Belchertown/graphs.conf
 sudo cp kiosk.css /etc/weewx/skins/Belchertown/kiosk.css
 sudo cp header.html.tmpl /etc/weewx/skins/Belchertown/header.html.tmpl
 sudo cp kiosk.html.tmpl /etc/weewx/skins/Belchertown/kiosk.html.tmpl
+sudo cp index_hook_after_wind_table.inc /etc/weewx/skins/Belchertown/index_hook_after_wind_table.inc
 #sudo cp kiosk-extras.js /var/www/html/weewx/kiosk-extras.js
 #sudo cp sdr.py /etc/weewx/bin/user/sdr.py
 #sudo chown root:weewx /etc/weewx/weewx.conf
-#sudo chown root:weewx /etc/weewx/skins/Belchertown/skin.conf
+sudo chown root:weewx /etc/weewx/skins/Belchertown/skin.conf
 sudo chown root:weewx /etc/weewx/skins/Belchertown/graphs.conf
 sudo chown root:weewx /etc/weewx/skins/Belchertown/kiosk.css
 sudo chown root:weewx /etc/weewx/skins/Belchertown/header.html.tmpl
 sudo chown root:weewx /etc/weewx/skins/Belchertown/kiosk.html.tmpl
+sudo chown root:weewx /etc/weewx/skins/Belchertown/index_hook_after_wind_table.inc
 #sudo chown weewx:weewx /var/www/html/weewx/kiosk-extras.js
 #sudo chown root:weewx /etc/weewx/bin/user/sdr.py
 #vsudo systemctl restart weewx
